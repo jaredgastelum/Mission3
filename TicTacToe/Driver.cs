872671c89d65a264bb4d42986a428dd80de5a38f
Jared Gastelum <jaredgastelum@gmail.com>
2023-01-23 09:35:31 -0700

    starting the game board

---
+++ TicTacToe/Driver.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TicTacToe
+{
+    public class Driver
+    {
+        static void DriverClass(string[] args)
+        {
+            Console.WriteLine("Welcome to our tic tac toe game!");
+
+
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,24 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Jared");
+            char[,] arr = new char[3, 3];
+
+            //arr[0, 1] = 'x';//initialization  
+            //arr[1, 2] = 'x';
+            //arr[2, 0] = 'x';
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (arr[i, j] == ' ')
+                    {
+                        Console.Write(arr[i, j] + "_ ");
+                    }
+               
+                }
+                Console.WriteLine();//new line at each row  
+            }
         }
     }
 }
