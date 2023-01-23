8ba731ee042b79213d76eb69f05e58918e253c5a
jwilki23 <wilkinsonjacob22@gmail.com>
2023-01-23 08:57:59 -0700

    Hello Jared

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,7 +6,7 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jared");
         }
     }
 }
