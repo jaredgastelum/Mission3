fadd7d1aee3f29d68383e28eceee8f55b5d6b77a
jwilki23 <wilkinsonjacob22@gmail.com>
2023-01-25 08:41:48 -0700

    New board

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -9,16 +9,16 @@ namespace TicTacToe
         {
             Console.WriteLine("Welcome to our tic tac toe game!" + "\n");
 
-            int[,] board = new int[3, 3];
-            int pos = 1;
+            char[,] board = new char[3, 3];
+            char pos = '1';
 
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
                     board[i, j] = pos;
-                    pos = pos + 1;
-                    Convert.ToString(pos);
+                    pos++;
+                   
                     Console.Write(board[i, j]);
                     if (j < 2)
                     {
@@ -41,45 +41,44 @@ namespace TicTacToe
             
             
 
-            pos = 1;
+            pos = '1';
             for (int i = 0; i < 3; i++)
             {
                 
                 for (int j = 0; j < 3; j++)
                 {
                     board[i, j] = pos;
-                    pos = pos + 1;
-                    Convert.ToString(pos);
+                    pos++;
                     
 
                     switch (ans)
                     {
                         case 1:
-                            board[0, 0] = 0;
+                            board[0, 0] = 'X';
                             break;
                         case 2:
-                            board[0, 1] = 0;
+                            board[0, 1] = 'X';
                             break;
                         case 3:
-                            board[0, 2] = 0;
+                            board[0, 2] = 'X';
                             break;
                         case 4:
-                            board[1, 0] = 0;
+                            board[1, 0] = 'X';
                             break;
                         case 5:
-                            board[1, 1] = 0;
+                            board[1, 1] = 'X';
                             break;
                         case 6:
-                            board[1, 2] = 0;
+                            board[1, 2] = 'X';
                             break;
                         case 7:
-                            board[2, 0] = 0;
+                            board[2, 0] = 'X';
                             break;
                         case 8:
-                            board[2, 1] = 0;
+                            board[2, 1] = 'X';
                             break;
                         case 9:
-                            board[2, 2] = 0;
+                            board[2, 2] = 'X';
                             break;
                     }
 
