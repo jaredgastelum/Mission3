9022fb0494951a2c2c4720fcb7e4aac608d18dfa
jwilki23 <wilkinsonjacob22@gmail.com>
2023-01-25 20:28:16 -0700

    Solution

---
+++ TicTacToe/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace TicTacToe
+{
+    class Supporting
+    {
+    
+        public static void BoardPrint(char[,] board)
+        {
+            //char[,] board = new char[3, 3];
+            //char pos = '1';
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(board[i, j]);
+                    if (j < 2)
+                    {
+                        Console.Write(" | ");
+                    }
+                }
+
+                if (i < 2)
+                {
+                    Console.WriteLine("\n---------");//new line at each row 
+                }
+            }
+
+        }
+
+
+        public static bool Winner(char[,] board)
+        {
+            // Check for a winner by checking rows, columns, and diagonals
+            for (int i = 0; i < 3; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2] && board[i, 0] != ' ') return true;
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i] && board[0, i] != ' ') return true;
+            }
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2] && board[0, 0] != ' ') return true;
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0] && board[0, 2] != ' ') return true;
+            return false;
+        }
+    }
+}
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -18,72 +18,8 @@ namespace TicTacToe
                 {
                     board[i, j] = pos;
                     pos++;
-                   
-                    Console.Write(board[i, j]);
-                    if (j < 2)
-                    {
-                        Console.Write(" | ");
-                    }
-                }
-
-                if (i < 2)
-                {
-                  Console.WriteLine("\n---------");//new line at each row 
-                }
-            }
-
-
-            // check for winner in while loop
-
-            Console.WriteLine("\n");
-            Console.WriteLine("Player 1's turn (choose a number on the board): ");
-            int ans = Convert.ToInt32(Console.ReadLine());
-            
-            
-
-            pos = '1';
-            for (int i = 0; i < 3; i++)
-            {
-                
-                for (int j = 0; j < 3; j++)
-                {
-                    board[i, j] = pos;
-                    pos++;
-                    
-
-                    switch (ans)
-                    {
-                        case 1:
-                            board[0, 0] = 'X';
-                            break;
-                        case 2:
-                            board[0, 1] = 'X';
-                            break;
-                        case 3:
-                            board[0, 2] = 'X';
-                            break;
-                        case 4:
-                            board[1, 0] = 'X';
-                            break;
-                        case 5:
-                            board[1, 1] = 'X';
-                            break;
-                        case 6:
-                            board[1, 2] = 'X';
-                            break;
-                        case 7:
-                            board[2, 0] = 'X';
-                            break;
-                        case 8:
-                            board[2, 1] = 'X';
-                            break;
-                        case 9:
-                            board[2, 2] = 'X';
-                            break;
-                    }
 
                     Console.Write(board[i, j]);
-
                     if (j < 2)
                     {
                         Console.Write(" | ");
@@ -96,6 +32,56 @@ namespace TicTacToe
                 }
             }
 
+
+            // check for winner in while loop
+
+            Console.WriteLine("\n");
+            
+
+            pos = '1';
+            int turn = 0;
+            //for (int turn = 0; turn < 9; turn++)
+            while (Supporting.Winner(board) == false)
+            {
+                
+                // Ask for player's choice
+                Console.WriteLine("\nPlayer {0}, please enter your choice: ", (turn % 2) + 1);
+                int ans = Convert.ToInt32(Console.ReadLine());
+                        
+                switch (ans)
+                {
+                    case 1:
+                        board[0, 0] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 2:
+                        board[0, 1] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 3:
+                        board[0, 2] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 4:
+                        board[1, 0] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 5:
+                        board[1, 1] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 6:
+                        board[1, 2] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 7:
+                        board[2, 0] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 8:
+                        board[2, 1] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                    case 9:
+                        board[2, 2] = (turn % 2 == 0) ? 'X' : 'O';
+                        break;
+                }
+                Supporting.BoardPrint(board);
+                turn++;
+                Supporting.Winner(board);
+            }
         }
     }
 }
